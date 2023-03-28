9d1f70938db64a3c680a3f8d9380083864cd5948
gabrielvlad <gabriel.vlad@outlook.dk>
2023-03-28 23:01:10 +0200

    Changes:
    -changed the favicon to a cute pokemon ball
    -changed the title of the app
    -made the pokemon cards to be listed in a row, and expanded the listing to 12 from 4.

--- 4th semester Internet of Things/IT-WEB2X-S23/08_assignment_2_a_pokedex/src/components/Header.js
+++ 4th semester Internet of Things/IT-WEB2X-S23/08_assignment_2_a_pokedex/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
+import Pokedex from "../assets/images/pokedex.png";
 
 const Header = () => {
   return (
     <header>
-      <h1 className="header-title">Pokedex</h1>
+      <div className="app__header">
+        <Link to="/">
+          <img src={Pokedex} alt="Pokedex" />
+        </Link>
+      </div>
     </header>
   );
 };
