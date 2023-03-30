901a46d7a995b4f84f18d366e53de6a4da8a73d8
gabrielvlad <gabriel.vlad@outlook.dk>
2023-03-30 19:11:42 +0200

    centered the buttons for next and previous
    self explanatory commit

--- 4th semester Internet of Things/IT-WEB2X-S23/08_assignment_2_a_pokedex/src/components/Pagination.js
+++ 4th semester Internet of Things/IT-WEB2X-S23/08_assignment_2_a_pokedex/src/components/Pagination.js
@@ -1,10 +1,13 @@
 import React from "react";
+import "../styles/Pagination.css"; // Import the CSS file with the above styles
 
 function Pagination({ goToNextPage, goToPrevPage }) {
   return (
-    <div className="pagination">
-      {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
-      {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+    <div className="parent-container">
+      <div className="pagination">
+        {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+        {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+      </div>
     </div>
   );
 }
