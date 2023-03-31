2174bfc53196a08cbd5c2bc45faf1db7c5ddb9d2
gabrielvlad <gabriel.vlad@outlook.dk>
2023-04-01 01:27:48 +0200

    More aspect changes
    -Still experimenting with aspect changes but I think I am happy for now

--- 4th semester Internet of Things/IT-WEB2X-S23/08_assignment_2_a_pokedex/src/components/Footer.js
+++ 4th semester Internet of Things/IT-WEB2X-S23/08_assignment_2_a_pokedex/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
 import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer-test">
+      <a
+        href="https://github.com/gabrielvlad"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GitHubIcon style={{ color: "#fff" }} />
+      </a>
       <p>Developed by Gabriel Vlad</p>
     </footer>
   );
